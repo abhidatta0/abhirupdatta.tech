55e7e8d1f66b0d745dd4f7f69ce026b154a470f8
abhidatta0 <abhidatta146@gmail.com>
2023-08-23 15:41:45 +0530

    added about and name text

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,13 @@ function App() {
 
   return (
     <main  className='main-content'>
+      <h1 className='name'>Abhirup Datta</h1>
       <img src={MyPhoto}  className='my-img' alt="Photo of Me"/>
+      <p className='about'>
+        Web and Mobile app developer specifically in React-Native.
+        <br />
+        Previously worked at <a href="https://letstransport.in/" className='lt-link'>Letstransport</a>
+      </p>
       <LinksContainer />
     </main>
   )
